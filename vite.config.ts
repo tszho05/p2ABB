2f340622ceda6b372c4c6962b466aaa7ac517460
tszho05 <kite000505@gmail.com>
2026-05-06 12:24:59 +0800

    Fix GitHub Pages blank screen

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',
   root: '.',
   plugins: [react()],
   test: {
